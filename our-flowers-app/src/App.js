9e561a9557d910c71bb0dd06e3ee80dfebe6860d
Roshani Ravi <rravi90@uw.edu>
2022-04-12 14:40:14 -0700

    add

--- our-flowers-app/src/App.js
+++ our-flowers-app/src/App.js
@@ -11,28 +11,30 @@ import About from './About';
 //   import React, { Component } from 'react';
 // import ReactDOM, { render } from 'react-dom';
 // import App from './App';
-import {Route, Link } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 import _ from 'lodash';
 import './index.css'; //import css file!
 
 export default function Header() {
   return (
-      <div className="container">
-          {/* <Route exact path="/" component={Gallery} />
+    <div className="container">
+      {/* <Route exact path="/" component={Gallery} />
           <Route path="/search" component={Search} />
           <Route path="/about" component={About} /> */}
-          <nav
-            style={{
-              borderBottom: "solid 1px",
-              paddingBottom: "1rem",
-            }}
-          >
-            <Link to="/">Home</Link>
-              {' - '}
-              <Link to="/search">Search</Link>
-              {' - '}
-              <Link to="/about">About</Link>
-          </nav>
-      </div>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/">Home</Link>
+        {' - '}
+        <Link to="/search">Search</Link>
+        {' - '}
+        <Link to="/about">About</Link>
+        {' - '}
+        <Link to="/userprofile"> User Profile</Link>
+      </nav>
+    </div>
   );
 }
