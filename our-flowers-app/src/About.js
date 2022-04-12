fa71c886331544994a3f76168e660a8a9eee34a6
Roshani Ravi <rravi90@uw.edu>
2022-04-12 15:11:37 -0700

    about page

---
+++ our-flowers-app/src/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function About() {
+
+    return (
+        <div>
+            <h1 style={{ color: "#9CAEA9" }}>Welcome to Jasmine</h1>
+            <h2 style={{ color: "#A2D7D7" }}>About us</h2>
+            <p>Our mission is to aid in dismantling internalized eurocentric beauty standards
+                by allowing users to explore natural physical variance of our faces and bodies.
+            </p>
+        </div>
+    );
+} 
