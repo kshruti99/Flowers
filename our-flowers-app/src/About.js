953974006476a626b501e9b7f2fe72c53c4c51be
kshruti99 <shrutikompella@gmail.com>
2022-04-22 13:16:33 -0700

    about page changes

--- our-flowers-app/src/About.js
+++ our-flowers-app/src/About.js
@@ -6,7 +6,7 @@ export default function About() {
         <div>
             <h1 style={{ color: "#9CAEA9" }}>Welcome to Jasmine</h1>
             <h2 style={{ color: "#A2D7D7" }}>About us</h2>
-            <p>Our mission is to aid in dismantling internalized eurocentric beauty standards
+            <p style={{ color: "#A2D7D7" }}>Our mission is to aid in dismantling internalized eurocentric beauty standards
                 by allowing users to explore natural physical variance of our faces and bodies.
             </p>
         </div>
