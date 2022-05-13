eedf6add6d86791409c040b8687cff5102665037
Roshani Ravi <rravi90@uw.edu>
2022-05-13 10:43:21 -0700

    search

--- our-flowers-app/src/Header.js
+++ our-flowers-app/src/Header.js
@@ -13,23 +13,23 @@ import {
 import './index.css'; //import css file!
 
 export default function Header() {
-  return (
-    <Navbar color="light" light expand="md">
-                <NavbarBrand href="/">Jasmine</NavbarBrand>
-                    <Nav className="mr-auto" navbar>
-                        <NavItem>
-                            <NavLink href="/gallery">Home</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href="/search">Search</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href="https://k4vya.github.io/jasmine-landing/">About</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href="/userprofile">Profile</NavLink>
-                        </NavItem>
-                    </Nav>
-            </Navbar>
-  );
+    return (
+        <Navbar color="light" light expand="md">
+            <NavbarBrand href="/">Jasmine</NavbarBrand>
+            <Nav className="mr-auto" navbar>
+                <NavItem>
+                    <NavLink href="/gallery">Home</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink href="/search">Search</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink href="https://k4vya.github.io/jasmine-landing/">About</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink href="/userprofile">Profile</NavLink>
+                </NavItem>
+            </Nav>
+        </Navbar>
+    );
 }
--- our-flowers-app/src/Search.js
+++ our-flowers-app/src/Search.js
@@ -1,59 +1,63 @@
 import React from 'react';
 import { IoIosSearch } from 'react-icons/io';
 import { Component } from 'react';
+import { useEffect, useState } from 'react'; //import React Component
 
 
 const tagValues = [
-    {id: 1, name: 'big nose'},
-    {id: 2, name: 'bushy eyebrows'},
-    {id: 3, name: 'gapped teeth'},
-    {id: 4, name: 'heterochromia'},
-    {id: 5, name: 'hairy arms'},
-    {id: 6, name: 'heterochromia'},
-    {id: 7, name: 'chin acne'},
-    {id: 8, name: 'textured skin'},
-    {id: 9, name: 'rosacea'},
-    {id: 10, name: 'hooded eyes'},
-    {id: 11, name: 'brown skin'}
-    
+    { id: 1, name: 'big nose' },
+    { id: 2, name: 'bushy eyebrows' },
+    { id: 3, name: 'gapped teeth' },
+    { id: 4, name: 'heterochromia' },
+    { id: 5, name: 'hairy arms' },
+    { id: 6, name: 'heterochromia' },
+    { id: 7, name: 'chin acne' },
+    { id: 8, name: 'textured skin' },
+    { id: 9, name: 'rosacea' },
+    { id: 10, name: 'hooded eyes' },
+    { id: 11, name: 'brown skin' }
+
 ];
 
 
-function PillFilter() { 
-    
+
+function PillFilter() {
+
     const { search } = window.location;
     const query = new URLSearchParams(search).get('s');
     const filteredPosts = filterPosts(tagValues, query);
-    return ( 
+    return (
         <div>
-            <p style={{color: 'white'}} className = "sugg">Suggested Tags:</p>
-                <ul>
-                    {filteredPosts.map((tag)=>(
-                        <button className ="pill" key={tag.id}>{tag.name}</button>
-                    ))}
-                
-                </ul>
-            
+            <p style={{ color: 'white' }} className="sugg">Suggested Tags:</p>
+            <ul>
+                {filteredPosts.map((tag) => (
+                    <button className="pill" key={tag.id}>{tag.name}</button>
+                ))}
+
+            </ul>
+
         </div>
-        
+
     )
 }
-function SearchBar() { 
-    return(
-        <form action="/" method="get">
-        
-       
-        <input
-            type="text"
-            id="header-search"
-            placeholder="search"
-            name="s" 
-        />
-        <button type="submit" id='cancel-button'>cancel</button>
-        
-    </form>
-    )
-}
+
+// function SearchBar() {
+//     return (
+//         <form action="/" method="get">
+
+
+//             <input
+//                 type="text"
+//                 id="header-search"
+//                 placeholder="search"
+//                 name="s"
+//                 onChange={searchTyped}
+//             />
+//             <button type="submit" id='cancel-button'>cancel</button>
+
+//         </form>
+//     )
+// }
 
 const filterPosts = (tagValues, query) => {
     if (!query) {
@@ -66,16 +70,40 @@ const filterPosts = (tagValues, query) => {
     });
 };
 
-export default function Search() {
+export default function Search(props) {
+    const [galTag, setTagInp] = useState("");
+    const [galleryResults, getGalleryResults] = useState([]);
+    const searchTyped = e => {
+        setTagInp(e.target.value);
+    };
+
+    useEffect(() => {
+        const galleryResults = props.info.filter(imgTag =>
+            (imgTag.tag + '').indexOf('' + galTag) > -1
+        );
+        getGalleryResults(galleryResults);
+    }, [galTag, props.info]);
 
     return (
         <div>
-          
-            <SearchBar/>
-       
+
+            <form action="/" method="get">
+
+
+                <input
+                    type="text"
+                    id="header-search"
+                    placeholder="search"
+                    name="s"
+                    onChange={searchTyped}
+                />
+                <button type="submit" id='cancel-button'>cancel</button>
+
+            </form>
+
             <PillFilter />
-            
+
         </div>
     );
-} 
+}
 
