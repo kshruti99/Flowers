c757b5d50b517d1a053d174945435b85b8abb95f
Roshani Ravi <rravi90@uw.edu>
2022-05-12 19:19:50 -0700

    tags edited

--- our-flowers-app/src/GalleryCard.js
+++ our-flowers-app/src/GalleryCard.js
@@ -2,31 +2,31 @@
 // import{Redirect} from 'react-router-dom';
 // import { Button } from 'reactstrap';
 
-function GalleryCard(props) {  
-    const tagValues = [
-        {id: 1, name: 'big nose'},
-        {id: 2, name: 'bushy eyebrows'},
-        {id: 3, name: 'gapped teeth'},
-        {id: 4, name: 'heterochromia'},
-        {id: 5, name: 'hairy arms'},
-        {id: 6, name: 'heterochromia'},
-        {id: 7, name: 'chin acne'},
-        {id: 8, name: 'textured skin'},
-        {id: 9, name: 'rosacea'},
-        {id: 10, name: 'hooded eyes'},
-        {id: 11, name: 'brown skin'}
-    ];
-    //render individual card for one gallery image
-    return (    
-      <div key={props.gal.galid} className="card" className="card-columns">
-        <img className="card-img-top" src={props.gal.path} alt={props.gal.imgalt} />
-        <div className="card-body">
-            {props.gal.tagid.map((currTag, j) =>
-                <button key={j} className ="smallpill">{tagValues[currTag - 1].name}</button>
-            )}
-        </div>
+function GalleryCard(props) {
+  const tagValues = [
+    { id: 1, name: 'big nose' },
+    { id: 2, name: 'bushy eyebrows' },
+    { id: 3, name: 'gapped teeth' },
+    { id: 4, name: 'heterochromia' },
+    { id: 5, name: 'hairy arms' },
+    { id: 6, name: 'heterochromia' },
+    { id: 7, name: 'chin acne' },
+    { id: 8, name: 'textured skin' },
+    { id: 9, name: 'rosacea' },
+    { id: 10, name: 'hooded eyes' },
+    { id: 11, name: 'brown skin' }
+  ];
+  //render individual card for one gallery image
+  return (
+    <div key={props.gal.galid} className="card" className="card-columns">
+      <img className="card-img-top" src={props.gal.path} alt={props.gal.imgalt} />
+      <div className="card-body">
+        {props.gal.tagid.map((currTag, j) =>
+          <button key={j} className="pill-version">{tagValues[currTag - 1].name}</button>
+        )}
       </div>
-    );
+    </div>
+  );
 }
 
 export default GalleryCard;
